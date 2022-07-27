9b93fa08494d7cf353443704c276ae1cac746a13
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2022-07-27 21:34:41 +0300

    bug

--- pages/api/visitor.ts
+++ pages/api/visitor.ts
@@ -3,39 +3,34 @@ const fs= require('fs')
 const path= require('path')
 const filePath = path.join(process.cwd(), 'data/rate.json');
 
+let data = fs.readFileSync(filePath,'utf8')
 
 
 export  default async function handler (req:NextApiRequest,res:NextApiResponse) {
   
    
     if (req.body==='"visitor"') {
-        fs.readFile(filePath,'utf8',function(err,data){
-            if (err) {
+           
           
+            let date= new Date()
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            let year = date.getFullYear();
+            let theData= JSON.parse(data)
+            theData['visitors']=theData['visitors'] +1
+            theData['lastVisit']=`${day}-${month}-${year}`
+
+            fs.writeFile('data/rate.json',JSON.stringify(theData),function(err){
+                if (err) {
                 res.status(500).json({err})
-             
-                
-            }
-            else {
-                let date= new Date()
-                let day = date.getDate();
-                let month = date.getMonth() + 1;
-                let year = date.getFullYear();
-                let theData= JSON.parse(data)
-                theData['visitors']=theData['visitors'] +1
-                theData['lastVisit']=`${day}-${month}-${year}`
- 
-                fs.writeFile('data/rate.json',JSON.stringify(theData),function(err){
-                   if (err) {
-                   res.status(500).json({err})
-                    return
-                   }
-                })
-             
-                 res.status(200).send({ok:'ok'})
+               
+                }
+            })
+            
+            res.status(200).send({ok:'ok'})
               
-            }
-        })
+            
+       
       
     }
  
