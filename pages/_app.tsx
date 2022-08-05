e7fe5fa640bc13eeb6743f2064cbaf4d13dd7d7a
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2022-08-05 23:53:40 +0300

    header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import { AppProps } from "next/app"
 import '../styles/global.css'
 import React, {useState,createContext} from 'react'
 
- 
-import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import Grid from '@mui/material/Grid'
 import Header from './header'
 
 import {CookiesProvider} from 'react-cookie'
@@ -17,36 +17,41 @@ const [canVote,setCanVote]=useState(true)
 const [openLogIn,setOpenLogIn]=useState(false)
 const [visitor,setVisitor]=useState({admin:false,user:false})
   return (
-    <div id="root" >
-  
-    <>
-     <Box  sx={{
-        gridColumn:"1/4",
-        gridRow:"1/1",
-     }} >
-       <Header 
-        setOpenRate={setOpenRate}
-        canVote={canVote}
-        setOpenLogIn={setOpenLogIn}
-        visitor={visitor}/>
-        
-      </Box>
-      
-       <div style={{gridRow:"2/2",gridColumn:"1/4"}}>
-       <animateContext.Provider value={{openRate,setOpenRate
-                                       ,setCanVote,openLogIn
-                                       ,setOpenLogIn,visitor
-                                       ,setVisitor,canVote}}>
-          <CookiesProvider>
+    <div id="root">
 
-            <Component {...pageProps}/>
-          </CookiesProvider>
-        </animateContext.Provider>
-          
-      </div>
-             </>  
+            <Grid container 
+            rowGap={1}>
+                  <Grid item xs={12}>
 
+                    <Header 
+                    setOpenRate={setOpenRate}
+                    canVote={canVote}
+                    setOpenLogIn={setOpenLogIn}
+                    visitor={visitor}/>
+                  </Grid>
+                  <Grid item xs={12}>
+                     <Divider variant="middle" 
+                     className="divider"/>
+                  </Grid>
+                  <Grid item xs={12}>
+
+                    <animateContext.Provider value={{openRate,setOpenRate
+                                                    ,setCanVote,openLogIn
+                                                    ,setOpenLogIn,visitor
+                                                    ,setVisitor,canVote}}>
+                      <CookiesProvider>
+
+                        <Component {...pageProps}/>
+                      </CookiesProvider>
+                    </animateContext.Provider>
+                  </Grid>
+              
+                
+            </Grid>
     </div>
+
+
+  
             
   )
     
--- pages/header.tsx
+++ pages/header.tsx
@@ -8,6 +8,7 @@ import Person from '@material-ui/icons/Person'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/header.module.scss'
+import Grid from '@mui/material/Grid'
 interface iProps {setOpenRate:Function,canVote:boolean
                   ,setOpenLogIn:Function,visitor:{user:boolean,admin:boolean}}
 const Header =({setOpenRate,canVote,setOpenLogIn,visitor}:iProps)=>{
@@ -22,65 +23,93 @@ const Header =({setOpenRate,canVote,setOpenLogIn,visitor}:iProps)=>{
  
     return (
 
-          <Toolbar sx={
-          {
-             width:"100%",
-             display:"flex",
-             justifyContent:"space-between"
-          }
-        } >
-                  <Link href="/">
-                     <a style={{boxShadow:"0px 0px 0px transparent"}}
-                      className={styles.link}>
-                        <Image src="/static/logo.png"
-                           className="logo"
-                           alt="B3D"
-                           width={30} height={30}/> 
+          <Toolbar sx={{padding:'0px'}}>
+         <Grid container>
+            <Grid item container xs={12} sm={6}
+            padding={1}
+            paddingLeft={0}
+            paddingRight={0}
+            >
+
+               <Grid item xs={4}>
+
+                        <Link href="/">
+                           <a style={{boxShadow:"0px 0px 0px transparent"}}
+                           className={styles.link}>
+                              <Image src="/static/logo.png"
+                                 className="logo"
+                                 alt="B3D"
+                              
+                                width={50}
+                                height={50}
+                                 /> 
+                              </a>
+                        </Link>
+               </Grid>
+               <Grid item xs={4}>
+
+                        <a onClick={()=>handleRate()}
+                        className={canVote?
+                           styles.rateMe
+                           :''
+                        }
+                        
+                        >
+                              Rate Me
+                              <Icon >
+                                 <Star/>
+                              </Icon>
                         </a>
-                  </Link>
-               
-                  <a href ="https://www.linkedin.com/in/mohammad-ismael-755ba4199/"
-                   className={styles.link} >
-                        LinkedIn
-                        <Icon color="inherit" >
-                           <LinkedIn/>
-                        </Icon>
-                  </a>
-                  <a href="https://github.com/ab3adel"
-                  className={styles.link}>
-                        GitHub
-                        <Icon >
-                           <GitHub/>
-                        </Icon>
-                  </a>
-                  <a onClick={()=>handleRate()}
-                    className={canVote?
-                     styles.rateMe
-                     :''
-                  }
-                    
-                    >
-                        Rate Me
-                        <Icon >
-                           <Star/>
-                        </Icon>
-                  </a>
-                  {
-                     visitor&&visitor.user?
-                     <a>
-                        Welcome
-                        <Icon >
-                           <Person/>
-                     </Icon>
-                     </a>:
-                     <a onClick={()=>setOpenLogIn(true)}
-                   className={styles.link}>
-                     Log In
+               </Grid>
+               <Grid item xs={4} >
+
+               {
+                  visitor&&visitor.user?
+                  <a>
+                     Welcome
                      <Icon >
-                           <Person/>
-                     </Icon>
-                  </a>
-                  }
+                        <Person/>
+                  </Icon>
+                  </a>:
+                  <a onClick={()=>setOpenLogIn(true)}
+               className={styles.link}>
+                  Log In
+                  <Icon >
+                        <Person/>
+                  </Icon>
+               </a>
+               }
+               </Grid>
+            </Grid>
+            <Grid item container xs ={12} sm={6}
+            justifyContent="center"
+            padding={1}
+            paddingLeft={0}
+            paddingRight={0}>
+
+               <Grid item xs={4}>
+
+                        <a href ="https://www.linkedin.com/in/mohammad-ismael-755ba4199/"
+                        className={styles.link} >
+                              LinkedIn
+                              <Icon color="inherit" >
+                                 <LinkedIn/>
+                              </Icon>
+                        </a>
+               </Grid>
+               <Grid item xs={4}>
+                        <a href="https://github.com/ab3adel"
+                        className={styles.link}>
+                              GitHub
+                              <Icon >
+                                 <GitHub/>
+                              </Icon>
+                        </a>
+                  </Grid>      
+                     
+            </Grid>
+            </Grid>
+
           </Toolbar>
     )
 }
